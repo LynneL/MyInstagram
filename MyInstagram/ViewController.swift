b16b7ef2b28160d621f2108416dfb175e2c96f18
Lynne <lynne@Lynnes-MacBook-Pro.local>
2020-05-24 21:59:16 -0700

    setup project

---
+++ MyInstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyInstagram
+//
+//  Created by Lynne on 5/24/20.
+//  Copyright © 2020 Lynne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
